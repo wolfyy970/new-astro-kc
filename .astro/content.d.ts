d018e674ec223ae561c5586b58cda3a0d7236fc9
KC <79471844+wolfyy970@users.noreply.github.com>
2026-06-14 12:36:31 -0400

    chore: save local changes before cleanup

--- .astro/content.d.ts
+++ .astro/content.d.ts
@@ -162,7 +162,15 @@ declare module 'astro:content' {
 	};
 
 	type DataEntryMap = {
-		
+		"case-studies": Record<string, {
+  id: string;
+  body?: string;
+  collection: "case-studies";
+  data: InferEntrySchema<"case-studies">;
+  rendered?: RenderedContent;
+  filePath?: string;
+}>;
+
 	};
 
 	type AnyEntryMap = ContentEntryMap & DataEntryMap;
@@ -194,6 +202,6 @@ declare module 'astro:content' {
 		LiveContentConfig['collections'][C]['loader']
 	>;
 
-	export type ContentConfig = typeof import("../src/content.config.mjs");
+	export type ContentConfig = typeof import("../src/content.config.js");
 	export type LiveContentConfig = never;
 }
